3ef1ca3d03b98784717d16b73f846ec4f9508b11
Mohd Ehtisham Ansari <mohammadehtisham034@gmail.com>
2023-11-28 20:46:39 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,14 @@
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e4e948e41dmshde3d0e8de130208p13444cjsnfc28789ee879',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "d862dc1a4349168179db8113028a54a7";
